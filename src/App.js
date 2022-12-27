43f49a33437100b514f8047181df20212f56354e
Rafeed Nazmul <rafeed.n1@gmail.com>
2022-12-27 15:37:20 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    state = { inspo : ""}
+
+    componentDidMount(){
+        this.fetchInspo()
+    }
+    
+    fetchInspo = () => {
+        axios.get('https://type.fit/api/quotes')
+            .then((response) => {
+                const len = response.data.length;
+                const {text} = response.data[Math.floor(Math.random() * len)]
+                console.log(text)
+                this.setState({inspo: text})
+
+                
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+  render() {
+    const {inspo} = this.state
+    return (
+        <div className='app'>
+            <h1 className='title'>Inspo Genterator</h1>
+            <div className='box'>
+                <h1 className='.gradient_text'>{inspo}</h1>
+            </div> 
+                <button className='btn' onClick={this.fetchInspo}>
+                    <span>Ask for More</span>
+                </button>
+        </div>
+        
+           
+        
+    )
+  }
 }
 
-export default App;
+export default App
